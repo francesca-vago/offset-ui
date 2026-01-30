2d620b404d261faad23b4d4998b015fe656302e4
Francesca Vago <vago.francesca@gmail.com>
2026-01-30 12:32:42 +0100

    style: Button components style and view

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,46 +1,79 @@
 <script setup lang="ts">
 import Button from 'primevue/button'
-defineProps<{
-  label: string
-  icon?: string
-}>()
+type Variant = 'full' | 'outline' | 'text'
+type Size = 'sm' | 'md' | 'lg'
+
+withDefaults(
+  defineProps<{
+    variant?: Variant
+    size?: Size
+    label?: string
+    icon?: string
+    disabled?: boolean
+  }>(),
+  {
+    variant: 'full',
+    size: 'md',
+    disabled: false,
+  },
+)
+
 defineEmits<{
   click: [event: MouseEvent]
 }>()
 </script>
+
+<style scoped>
+.full {
+  background-color: var(--primary);
+}
+
+.outline {
+  background-color: white;
+}
+
+.text {
+  background-color: transparent;
+  border: none;
+  box-shadow: none;
+}
+
+.sm {
+  padding: 0.25rem 0.75rem;
+  font-size: 0.75rem;
+  line-height: 1rem;
+}
+
+.md {
+  padding: 0.5rem 1.5rem;
+  font-size: 1rem;
+}
+
+.lg {
+  padding: 0.5rem 2.5rem;
+  font-size: 1.25rem;
+}
+
+.disabled {
+  cursor: not-allowed;
+  background-color: var(--color-neutral-200);
+  color: black;
+  opacity: 0.8;
+}
+
+.icon-only {
+  padding: 0.25rem 0.5rem;
+}
+</style>
 
 <template>
   <Button
     :label="label"
     @click="$emit('click', $event)"
     :icon="icon"
-    pt:root="h-12 border-black border-2 p-2.5 bg-[#A6FAFF] hover:bg-[#79F7FF] hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF] cursor-pointer"
-    pt:label="font-bold text-sm"
-    pt:icon="text-white text-xl"
+    :disabled="disabled"
+    pt:root="h-fit space-x-2 border-black border-1 hover:bg-[#79F7FF] shadow-[4px_4px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF] active:shadow-none cursor-pointer rounded-sm"
+    pt:label="font-bold"
+    :class="[variant, size, disabled ? 'disabled' : '', icon && !label ? 'icon-only' : '']"
   />
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
--- src/views/ButtonsView.vue
+++ src/views/ButtonsView.vue
@@ -3,7 +3,34 @@ import Button from '../components/Button.vue'
 </script>
 
 <template>
-  <h2>Buttons</h2>
-  <Button label="Click me" />
-  <Button label="Click me" icon="pi pi-check" />
+  <h1>Buttons</h1>
+
+  <h2>Button Variants</h2>
+  <div class="flex gap-2">
+    <Button label="Click me" variant="full" />
+    <Button label="Click me" variant="outline" />
+    <Button label="Click me" variant="text" />
+  </div>
+
+  <h2>Button Sizes</h2>
+  <div class="flex gap-2">
+    <Button label="Click me" size="sm" />
+    <Button label="Click me" size="md" />
+    <Button label="Click me" size="lg" />
+  </div>
+
+  <h2>Button with Icon</h2>
+  <div>
+    <Button label="Click me" icon="pi pi-check" />
+  </div>
+
+  <h2>Button Disabled</h2>
+  <div class="flex gap-2">
+    <Button label="Click me" disabled />
+  </div>
+
+  <h2>Icon only</h2>
+  <div>
+    <Button icon="pi pi-check" />
+  </div>
 </template>
